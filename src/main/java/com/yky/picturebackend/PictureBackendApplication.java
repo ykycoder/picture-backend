6319494d4335b84f1cfffd378bbad132d6e704c2
ykycoder <649074096@qq.com>
2024-12-10 21:59:37 +0800

    项目初始化

---
+++ src/main/java/com/yky/picturebackend/PictureBackendApplication.java
@@ -0,0 +1,17 @@
+package com.yky.picturebackend;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@SpringBootApplication
+@MapperScan("com.yky.picturebackend.mapper")
+@EnableAspectJAutoProxy(exposeProxy = true)
+public class PictureBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PictureBackendApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/yky/picturebackend/controller/MainController.java
@@ -0,0 +1,21 @@
+package com.yky.picturebackend.controller;
+
+import com.yky.picturebackend.common.BaseResponse;
+import com.yky.picturebackend.common.ResultUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class MainController {
+
+    /**
+     * 健康检查
+     * @return
+     */
+    @GetMapping("/health")
+    public BaseResponse<String> health() {
+        return ResultUtils.success("ok");
+    }
+}
