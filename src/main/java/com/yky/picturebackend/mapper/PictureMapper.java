7943d47eacbfaf8b24cdeb81d4583272defbb95e
ykycoder <649074096@qq.com>
2024-12-16 22:16:14 +0800

    图片模块开发

---
+++ src/main/java/com/yky/picturebackend/mapper/PictureMapper.java
@@ -0,0 +1,18 @@
+package com.yky.picturebackend.mapper;
+
+import com.yky.picturebackend.model.entity.Picture;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author yky
+* @description 针对表【picture(图片)】的数据库操作Mapper
+* @createDate 2024-12-15 14:23:23
+* @Entity com.yky.picturebackend.model.entity.Picture
+*/
+public interface PictureMapper extends BaseMapper<Picture> {
+
+}
+
+
+
+
---
+++ src/main/java/com/yky/picturebackend/service/PictureService.java
@@ -0,0 +1,66 @@
+package com.yky.picturebackend.service;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.yky.picturebackend.model.dto.picture.PictureQueryRequest;
+import com.yky.picturebackend.model.dto.picture.PictureUploadRequest;
+import com.yky.picturebackend.model.entity.Picture;
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.yky.picturebackend.model.entity.User;
+import com.yky.picturebackend.model.vo.PictureVO;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author yky
+ * @description 针对表【picture(图片)】的数据库操作Service
+ * @createDate 2024-12-15 14:23:23
+ */
+public interface PictureService extends IService<Picture> {
+
+    /**
+     * 校验图片
+     *
+     * @param picture
+     */
+    void validPicture(Picture picture);
+
+    /**
+     * 上传图片
+     *
+     * @param multipartFile
+     * @param pictureUploadRequest
+     * @param loginUser
+     * @return
+     */
+    PictureVO uploadPicture(MultipartFile multipartFile,
+                            PictureUploadRequest pictureUploadRequest,
+                            User loginUser);
+
+    /**
+     * 获取图片包装类（单条）
+     *
+     * @param picture
+     * @param request
+     * @return
+     */
+    PictureVO getPictureVO(Picture picture, HttpServletRequest request);
+
+    /**
+     * 获取图片包装类（分页）
+     *
+     * @param picturePage
+     * @param request
+     * @return
+     */
+    Page<PictureVO> getPictureVOPage(Page<Picture> picturePage, HttpServletRequest request);
+
+    /**
+     * 获取查询对象
+     *
+     * @param pictureQueryRequest
+     * @return
+     */
+    QueryWrapper<Picture> getQueryWrapper(PictureQueryRequest pictureQueryRequest);
+}
